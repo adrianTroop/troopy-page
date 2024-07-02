8c925a69629759c02a79a5b75c644ded802d672a
Adrian <adrian@adriantroop.com>
2024-07-02 12:29:31 +0200

    event handled

--- test/Lottery.js
+++ test/Lottery.js
@@ -1,5 +1,6 @@
 const { expect } = require("chai")
 const { ethers } = require("hardhat")
+const { result } = require("lodash")
 
 const tokens = (n) => {
     return ethers.utils.parseUnits(n.toString(), "ether")
@@ -7,6 +8,7 @@ const tokens = (n) => {
 
 describe("LotteryContract", ()=> {
     let accounts, user;
+    let transaction, result;
     let amount = tokens(10);
     //let lotteryContract;
     beforeEach(async ()=> {
@@ -17,7 +19,8 @@ describe("LotteryContract", ()=> {
         accounts = await ethers.getSigners();
         user = accounts[0]
         //makes a deposit
-        lotteryContract.connect(user).deposit({ value: amount })
+        transaction = await lotteryContract.connect(user).deposit({ value: amount })
+        result = await transaction.wait()
     })
     console.log(accounts)
     describe("Deposit Tokens and check balance", () =>{
@@ -31,7 +34,11 @@ describe("LotteryContract", ()=> {
             expect(lotteryContract.connect(user).deposit({ value: amount })).to.be.reverted;
         })
         it("Emit deposit event", async ()=>{
-            
+            const event = result.events[0]
+            const args = event.args
+            expect(event.event).to.equal('Deposit')
+            expect(args.user).to.equal(user.address)
+            expect(args.amount).to.equal(amount)
         })
     })
 })
